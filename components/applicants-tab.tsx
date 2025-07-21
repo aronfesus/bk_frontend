82b751a5a301e313af4f56993d89c4f984c679a8
Áron Fésüs <aronfesus@gmail.com>
2025-07-21 21:19:09 +0200

    3row

--- components/applicants-tab.tsx
+++ components/applicants-tab.tsx
@@ -395,7 +395,7 @@ export function ApplicantsTab() {
         </Select>
       </div>
 
-      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
         {filteredApplicants.map((applicant) => (
           <Card
             key={applicant.applicantId}
